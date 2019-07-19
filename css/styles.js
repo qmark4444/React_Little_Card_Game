c20ef7caf12cbb31e5d915beb707f60f9e1784c7
qmark4444 <qmarklong@gmail.com>
2019-07-19 10:51:09 -0400

    add more styles

--- css/styles.js
+++ css/styles.js
@@ -1,6 +1,17 @@
+export const cardStyle = {
+    position: "absolute",
+    border: "2px solid yellow",
+    height: 100,
+    width: 70,
+    padding: 10,
+    borderRadius: 10,
+    cursor: 'pointer',
+    textAlign: 'center',
+}
+
 export const scalingFactors = { 
     normal: 45, 
-    big: 30 
+    big: 30
 };
 
 export const colors = {
@@ -9,7 +20,9 @@ export const colors = {
     green: "#65ed99",
     tan: "#FFEFE8",
     blue: "#5DA9E9",
-    gray1: "#888888"        
+    gray1: "#888888",
+    red: "#ff0000",
+    white: "#ffffff"        
 };
 
 export const fonts = {
@@ -22,4 +35,14 @@ export const headerStyle = {
     position: 'relative',
     top: 80,
     margin: 0,
+}
+
+export const inputButtonStyle = {
+    justifyContent: "center",
+    padding: 10,
+    margin: 10,
+    fontSize: 24,
+    alignItems: "center", 
+    backgroundColor: 'green',
+    color: 'white'
 }
